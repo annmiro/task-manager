25a38f08fee020b88b24ba5f3067078f7731d106
annmiro <a.knyazevav@yandex.ru>
2024-01-19 16:41:36 +0400

    migrated from CRA to Vite

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import './Header.styles.css';
-import { ReactComponent as MoonIcon } from '../../images/moon.svg';
+import MoonIcon from "./moon.svg?react";
+
 
 function Header() {
     return <header className="header">
         <h1 className='header-logo'>Personal <br />Task Manager</h1>
         <button type='button' className='header-toggle-theme-button'><MoonIcon /></button>
     </header>
-};
+}
 
 export default Header;
