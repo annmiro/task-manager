b2f0ae8cdd72ecb7b48d9121934f275197302bb7
annmiro <a.knyazevav@yandex.ru>
2024-06-05 22:39:43 +0400

    added reset category button

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -12,22 +12,22 @@ import { TaskContext } from '../../context/Tasks/TaskContext';
 function Main() {
     const { updateQueryParams } = useContext(TaskContext);
 
-    const setFilter = (category) => {
-        updateQueryParams({ category });
-    };
-
     return <main className="main">
         <Sidebar name="Categories">
             <List gap={10} direction="column">
                 {categories.map(category => (
                     <ListItem key={category}>
-                        <Button variant='ghost' onClick={() => setFilter(category)}>
+                        <Button variant='ghost' onClick={() => updateQueryParams({ category })}>
                             <Tag status={category} size='l'></Tag>
                         </Button>
                     </ListItem>
                 ))}
             </List>
+            <div className='reset-button-wrapper'>
+                <Button variant='primary' onClick={() => updateQueryParams({ category: '' })}>Reset</Button>
+            </div>
         </Sidebar>
+
         <MainContent />
     </main>
 }
